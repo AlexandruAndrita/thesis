aac4a6b8bf0b847aec467227b80309123cb51f49
Alexandru Andrita <andu.andritza@gmail.com>
2023-12-19 02:10:07 +0200

    data loaded with stacking and padding

---
+++ group_dataset_images/group_images.py
@@ -0,0 +1,97 @@
+import os
+import torch
+import glob
+from PIL import Image
+import numpy as np
+from typing import Optional
+
+from prep_dataset import to_grayscale
+from prep_dataset import prepare_image
+
+class RandomImagePixelationDataset(torch.utils.data.Dataset):
+
+    def __init__(
+            self,
+            image_dir,
+            width_range: tuple[int,int],
+            height_range: tuple[int,int],
+            size_range: tuple[int,int],
+            dtype: Optional[type]=None
+    ):
+        self.validate_input_directory(image_dir)
+        self.image_dir=image_dir
+
+        self.validate_inputs(width_range, height_range, size_range)
+        self.width_range = width_range
+        self.height_range = height_range
+        self.size_range = size_range
+
+
+        self.filenames=self.get_files()
+        self.dtype=dtype
+
+    def get_files(self):
+        self.image_dir = os.path.abspath(self.image_dir)
+        files = glob.glob(os.path.join(self.image_dir, "**", "*"), recursive=True)
+        filenames = list()
+        for f in files:
+            if os.path.isfile(f):
+                name, extension = os.path.splitext(f)
+                if extension in [".jpg", ".jpeg", ".JPG", ".JPEG"]:
+                    filenames.append(f)
+        filenames.sort()
+
+        return filenames
+
+    @staticmethod
+    def validate_input_directory(directory):
+        if os.path.exists(directory) is False or os.path.isdir(directory) is False:
+            raise ValueError("Image directory is not a valid directory")
+
+    @staticmethod
+    def validate_inputs(width,height,size):
+        if width[0]<2:
+            raise ValueError("Minimum width cannot be smaller than 2")
+        if height[0]<2:
+            raise ValueError("Minimum height cannot be smaller than 2")
+        if size[0]<2:
+            raise ValueError("Minimum size cannot be smaller than 2")
+        if width[0]>width[1]:
+            raise ValueError("Minimum width cannot be greater than maximum width")
+        if height[0]>height[1]:
+            raise ValueError("Minimum height cannot be greater than maximum height")
+        if size[0]>size[1]:
+            raise ValueError("Minimum size cannot be greater than maximum size")
+
+
+    def __getitem__(self, index):
+        image_file=self.filenames[index]
+        image_loaded=Image.open(image_file)
+        image_array=np.array(image_loaded)
+        image_array=image_array.astype(dtype=self.dtype)
+        grayscale_image=to_grayscale.to_grayscale(image_array)
+
+        rng=np.random.default_rng(seed=index)
+        width=rng.integers(low=self.width_range[0],high=self.width_range[1]+1,size=1)[0]
+        height=rng.integers(low=self.height_range[0],high=self.height_range[1]+1,size=1)[0]
+
+        # (1,H,W)
+        if width>grayscale_image.shape[2]:
+            width=grayscale_image.shape[2]
+        if height>grayscale_image.shape[1]:
+            height=grayscale_image.shape[1]
+
+        x=rng.integers(low=0,high=grayscale_image.shape[2]-width+1,size=1)[0]
+        y=rng.integers(low=0,high=grayscale_image.shape[1]-height+1,size=1)[0]
+
+        size=rng.integers(low=self.size_range[0],high=self.size_range[1]+1,size=1)[0]
+
+        pixelated_image,known_array,target_array=prepare_image.prepare_image(grayscale_image,x,y,width,height,size)
+
+        return pixelated_image,known_array,target_array,image_file
+
+
+    def __len__(self):
+        return len(self.filenames)
+
+
---
+++ group_dataset_images/stacking_method.py
@@ -0,0 +1,67 @@
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+def get_max_height_width(batch_as_list: list):
+    max_width=float("-inf")
+    max_height=float("-inf")
+
+    # (1,H,W)
+    for image in batch_as_list:
+        width=image[0].shape[2]
+        height=image[0].shape[1]
+
+        max_width=max(max_width,width)
+        max_height=max(max_height,height)
+
+    return max_height,max_width
+
+def apply_padding(array,max_height,max_width,c_value):
+    if array.shape[0]<max_height:
+        padding_dimension_height=max_height-array.shape[0]
+        array=np.pad(array,((0,padding_dimension_height),(0,0)),mode='constant',constant_values=c_value)
+
+    if array.shape[1]<max_width:
+        padding_dimension_width=max_width-array.shape[1]
+        array=np.pad(array,((0,0),(0,padding_dimension_width)),mode='constant',constant_values=c_value)
+
+    return array
+
+def stack_with_padding(batch_as_list: list):
+    # (pixelated_image, known_array, target_array, image_file)
+
+    max_height,max_width=get_max_height_width(batch_as_list)
+
+    for i,image in enumerate(batch_as_list):
+        pixelated_image,known_array,target_array,image_file=image
+
+        original_height_pix_image=pixelated_image.shape[1]
+        original_width_pix_image=pixelated_image.shape[2]
+
+        original_height_known_array=known_array.shape[1]
+        original_width_known_array=known_array.shape[2]
+
+        pixelated_image=pixelated_image.reshape(original_height_pix_image,original_width_pix_image)
+        known_array=known_array.reshape(original_height_known_array,original_width_known_array)
+
+        pixelated_image=apply_padding(pixelated_image,max_height,max_width,0)
+        known_array=apply_padding(known_array,max_height,max_width,1)
+
+        pixelated_image=pixelated_image.reshape(1,max_height,max_width)
+        known_array=known_array.reshape(1,max_height,max_width)
+
+        batch_as_list[i]=(pixelated_image,known_array,target_array,image_file)
+
+
+    stacked_pixelated_images=np.stack([image[0] for image in batch_as_list],axis=0)
+    stacked_pixelated_images=torch.tensor(stacked_pixelated_images)
+
+    stacked_known_arrays=np.stack([image[1] for image in batch_as_list],axis=0)
+    stacked_known_arrays=torch.tensor(stacked_known_arrays)
+
+    target_arrays=[torch.tensor(image[2]) for image in batch_as_list]
+
+    image_files=[image[3] for image in batch_as_list]
+
+    return stacked_pixelated_images,stacked_known_arrays,target_arrays,image_files
+
