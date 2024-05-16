7373d5f0a55484ad3295f4df6fe69b7e13a8e63e
Alexandru Andrita <andu.andritza@gmail.com>
2024-05-16 05:32:13 +0300

    models_experiments and new model in the app

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import os
-from flask import Flask, render_template, request, redirect, flash
-from evaluation.model_file import CNNModel
-from PIL import Image
 from werkzeug.utils import secure_filename
 import io
 import base64
+from io import BytesIO
+
+from flask import Flask, render_template, request, redirect, flash
+import pickle
+
+from PIL import Image
 import numpy as np
 import torch
+from sklearn.neighbors import KNeighborsRegressor
 from torchvision import transforms
-from io import BytesIO
+
+from evaluation.model_file import CNNModel
 from prep_dataset import to_grayscale
+from evaluation.train_individual_model import find_model_output
 
 
 app = Flask(__name__)
@@ -21,6 +27,8 @@ model.eval()
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['jpg', 'jpeg', "JPG", "JPEG"]
 
+def allowed_pickle(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ['pkl']
 
 def save_image(image, filename):
     downloads_dir = os.path.join(os.path.expanduser('~'), 'Downloads')
@@ -37,43 +45,59 @@ def upload_file():
             flash('No file part')
             return render_template('index.html')
         file = request.files['file']
+        pickle_file = request.files['pickle_file']
         if file.filename == '':
-            flash('No selected file')
+            flash('No image selected')
             return render_template('index.html')
-        if file and allowed_file(file.filename):
+        if pickle_file.filename == '':
+            flash('No boolean mask selected')
+            return render_template('index.html')
+        if file and allowed_file(file.filename) and allowed_pickle(pickle_file.filename):
             filename = secure_filename(file.filename)
             file_content = file.read()
+            pickle_content = pickle_file.read()
+            encoded_pickle = base64.b64encode(pickle_content).decode('utf-8')
             encoded_img = base64.b64encode(file_content).decode('utf-8')
-            return render_template('index.html', filename=encoded_img)
+            return render_template('index.html', filename=encoded_img, pickle_file=encoded_pickle)
         else:
             flash('Invalid file type.')
     return render_template('index.html')
 
-
 @app.route('/process', methods=['POST'])
 def process_image():
     filename = request.form['filename']
-    preprocess = transforms.Compose([
-        transforms.Resize((128, 170)),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485], std=[0.229])
-    ])
+    pickle_file = request.form['pickle_file']
+    pickle_bytes = base64.b64decode(pickle_file)
+    known_array = pickle.loads(pickle_bytes)
+    # preprocess = transforms.Compose([
+    #     transforms.Resize((128, 170)),
+    #     transforms.ToTensor(),
+    #     # transforms.Normalize(mean=[0.485], std=[0.229])
+    # ])
     image = base64.b64decode(filename)
     img = Image.open(io.BytesIO(image))
     np_arr = np.array(img)
     grayscale_img = to_grayscale.to_grayscale(np_arr)
-    grayscale_img = grayscale_img.squeeze(0)
-    grayscale_img_pil = Image.fromarray(grayscale_img)
-    img_tensor = preprocess(grayscale_img_pil)
-    with torch.no_grad():
-        output_tensor = model(img_tensor)
-
-    output_tensor = output_tensor.squeeze(0)
-    processed_output = output_tensor.cpu().detach().numpy()
-    processed_img_pil = Image.fromarray((processed_output * 255).astype(np.uint8))
+    grayscale_img = grayscale_img.reshape(grayscale_img.shape[1],grayscale_img.shape[2],1).mean(axis=2)
+    known_array = known_array.squeeze(0)
+    final_image = find_model_output(
+        regressor=KNeighborsRegressor(n_neighbors=10, weights='distance'),
+        known_array = known_array,
+        image = grayscale_img
+    )
+    final_image_pil = Image.fromarray(final_image.astype(np.uint8))
+    # grayscale_img = grayscale_img.squeeze(0)
+    # grayscale_img_pil = Image.fromarray(grayscale_img)
+    # img_tensor = preprocess(grayscale_img_pil)
+    # with torch.no_grad():
+    #     output_tensor = model(img_tensor)
+    #
+    # output_tensor = output_tensor.squeeze(0)
+    # processed_output = output_tensor.cpu().detach().numpy()
+    # processed_img_pil = Image.fromarray((processed_output * 255).astype(np.uint8))
 
     buffered = BytesIO()
-    processed_img_pil.save(buffered, format="JPEG")
+    final_image_pil.save(buffered, format="JPEG")
     processed_img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
     return render_template('index.html', filename=filename, processed_filename=processed_img_str)
