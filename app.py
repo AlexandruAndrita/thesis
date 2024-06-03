c2ef26bc5a3dd5361814fbc7cacce6c5ac924a73
Alexandru Andrita <andu.andritza@gmail.com>
2024-06-03 21:44:25 +0300

    save adjusted for web usage too

--- app.py
+++ app.py
@@ -42,10 +42,16 @@ def prepare_image_for_interface(image):
 
 
 def save_image(image, filename):
-    downloads_dir = os.path.join(os.path.expanduser('~'), 'Downloads')
-    image_path = os.path.join(downloads_dir, filename)
+    images_directory = os.path.join(os.getcwd(), 'processed_images')
+    if not os.path.exists(images_directory):
+        os.makedirs(images_directory)
+
+    # downloads_dir = os.path.join(os.path.expanduser('~'), 'Downloads')
+
+    image_path = os.path.join(images_directory, filename)
     with open(image_path, 'wb') as f:
         f.write(image)
+
     return image_path
 
 
